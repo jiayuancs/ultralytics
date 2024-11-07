3748c5ecc7bab484665fda70a23ca2bd506bbec6
jiayuancs <jiayuancs@yeah.net>
2024-11-07 19:07:19 +0800

    chore: update README.md

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from ultralytics import YOLO
 def get_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', default='ChinaMobile9000.yaml', type=str, help="数据集")
-    parser.add_argument('--cfg', default='yolo11-tiny.yaml', type=str, help="模型结构")
+    parser.add_argument('--cfg', default='yolo11n-tiny.yaml', type=str, help="模型结构")
     parser.add_argument('--epochs', default=300, type=int)
     parser.add_argument('--imgsz', default=640, type=int)
     parser.add_argument('--batch', default=32, type=int)
@@ -22,6 +22,8 @@ def training(args):
     assert os.path.exists(datapath), "dataset yaml file not found"
 
     model = YOLO(model=args.cfg, task="detect")
+
+    # 参数的含义具体见 https://docs.ultralytics.com/modes/train/#train-settings
     model.train(
         data=datapath,
         epochs=args.epochs,
